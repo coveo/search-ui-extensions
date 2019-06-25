eb4cb69fc9ddb30d22de11b632a131143f680535
Michael Grondines <mgrondines@coveo.com>
2019-06-25 13:46:15 -0400

    Review modifications Adjusted Prettier options Added option to set a specific Field Renamed for a more generic approach SNOW-142

--- src/Index.ts
+++ src/Index.ts
@@ -1,6 +1,4 @@
 // This entry point defines all the components that are included in the extensions.
 
 export { AttachResult } from './components/AttachResult/AttachResult';
-export {
-    FilterAttachedResults
-} from './components/FilterAttachedResults/FilterAttachedResults';
+export { ResultsFilter } from './components/ResultsFilter/ResultsFilter';
