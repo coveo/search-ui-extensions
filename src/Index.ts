cc6ec87ea802e66cca67de4e5fe7429bc8255b8f
Michael Grondines <mic.grondines@gmail.com>
2019-06-27 08:23:19 -0400

    Filter Attached Results component (#3)
    Added small component to filter results depending on a specific field
    SNOW-142

--- src/Index.ts
+++ src/Index.ts
@@ -1,3 +1,4 @@
 // This entry point defines all the components that are included in the extensions.
 
 export { AttachResult } from './components/AttachResult/AttachResult';
+export { ResultsFilter } from './components/ResultsFilter/ResultsFilter';
