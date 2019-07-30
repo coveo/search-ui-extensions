3e90fc9a046a1f308274ff1eb31527dfe6a5dfe2
jeremierobert-coveo <43446516+jeremierobert-coveo@users.noreply.github.com>
2019-07-30 15:27:14 -0400

    Fully support es6 (#18)

--- scripts/dev.server.js
+++ scripts/dev.server.js
@@ -11,7 +11,7 @@ const compiler = webpack(webpackConfig);
 
 var server = new WebpackDevServer(compiler, {
     contentBase: ['bin/', 'node_modules/coveo-search-ui/bin/'],
-    publicPath: '/js/commonjs',
+    publicPath: '/commonjs',
     compress: true
 });
 server.listen(port, 'localhost', function() {});
