61345757869eb004c06ef296cc731ea74b8f5fa6
Jeremie Robert <jrobert@coveo.com>
2019-07-31 11:10:08 -0400

    Put back before deploy Script path

---
+++ scripts/before.deploy.js
@@ -0,0 +1,15 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { resolve } = require('path');
+
+const TAG = process.env.TRAVIS_TAG;
+if (!TAG) {
+    throw new Error('TRAVIS_TAG is not define in the current environment');
+}
+
+const VERSION = TAG.slice(TAG.indexOf('v') + 1, TAG.length);
+const PACKAGE_JSON_LOCATION = resolve('package.json');
+
+const PACKAGE = JSON.parse(readFileSync(PACKAGE_JSON_LOCATION, { encoding: 'utf8' }));
+PACKAGE['version'] = VERSION;
+
+writeFileSync(PACKAGE_JSON_LOCATION, JSON.stringify(PACKAGE, null, 4), { encoding: 'utf8' });
