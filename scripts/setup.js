65d701105fd3584d4275054ef610e918b6c6db53
Jeremie Robert <jrobert@coveo.com>
2019-07-31 10:50:44 -0400

    Fix missing svg issue lint

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,15 +1,16 @@
 'use strict';
-const ncp = require('ncp').ncp;
+const { ncp } = require('ncp');
+const { resolve } = require('path');
+const { mkdirSync, existsSync } = require('fs');
 
-const folderToCopy = [
-    {
-        src: './pages',
-        dest: './bin'
-    },
-    {
-        src: './img',
-        dest: './bin/img'
-    }
-];
+const folderToCopy = [{ src: resolve('./pages'), dest: resolve('./bin/pages') }, { src: resolve('./img'), dest: resolve('./bin/img') }];
 
-folderToCopy.forEach(folder => ncp(folder.src, folder.dest));
+if (!existsSync(resolve('./bin'))) {
+    mkdirSync(resolve('./bin'), { recursive: true });
+}
+
+folderToCopy.forEach(folder =>
+    ncp(folder.src, folder.dest, err => {
+        if (err) throw err;
+    })
+);
