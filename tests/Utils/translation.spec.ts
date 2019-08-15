ab98a1b597d747dcacf78fa65ed7bccecc284f53
Jeremie Robert <jrobert@coveo.com>
2019-08-15 17:18:49 -0400

    Coverage insanity SFINT-2448

--- tests/Utils/translation.spec.ts
+++ tests/Utils/translation.spec.ts
@@ -3,23 +3,40 @@ import { Translation, Language } from '../../src/utils/translation';
 describe('Translation', () => {
     describe('register', () => {
         it('should register each translation in the String locales', () => {
-            const dict = {
-                1: 'someTest1',
-                2: ''
+            (String as any)['locales'] = {};
+            (String as any)['locales']['en'] = {};
+            const dict: { [key: string]: string } = {
+                '1': 'someTest1',
+                '2': ''
             };
             Translation.register(Language.English, dict);
 
             Object.keys(dict).forEach(key => {
-                expect(String['locales'][Language.English][key]).toBe(dict[key]);
+                expect((String as any)['locales'][Language.English][key]).toBe(dict[key]);
                 expect(key.toLocaleString()).toBe(dict[key]);
             });
-            expect(String['locales'][Language.English][3]).toBe(undefined);
+            expect((String as any)['locales'][Language.English][3]).toBe(undefined);
+        });
+
+        it('should register each translation in the String locales even when the String locales is not defined yet', () => {
+            (String as any)['locales'] = undefined;
+            const dict: { [key: string]: string } = {
+                '1': 'someTest1',
+                '2': ''
+            };
+            Translation.register(Language.English, dict);
+
+            Object.keys(dict).forEach(key => {
+                expect((String as any)['locales'][Language.English][key]).toBe(dict[key]);
+                expect(key.toLocaleString()).toBe(dict[key]);
+            });
+            expect((String as any)['locales'][Language.English][3]).toBe(undefined);
         });
 
         it('should enable toLocaleString for each registered strings', () => {
-            const dict = {
-                1: 'someTest1',
-                2: ''
+            const dict: { [key: string]: string } = {
+                '1': 'someTest1',
+                '2': ''
             };
             Translation.register(Language.English, dict);
 
