29d88451ec7cbff61c0f2987c6c9e4266133781f
William O'Sullivan-Dargis <wosullivan@coveo.com>
2020-10-02 15:44:24 -0400

    Added comment for the onClick option to specify that the option must be set in JS

--- src/components/TopQueries/TopQueries.ts
+++ src/components/TopQueries/TopQueries.ts
@@ -26,6 +26,8 @@ export interface ITopQueriesOptions {
      * Specifies the handler called when a suggestion is clicked.
      *
      * Default executes a search query using the suggestion
+     * 
+     * This option must be set in JavaScript when initializing the component.
      */
     onClick?: (expression: string, component: TopQueries) => void;
 }
