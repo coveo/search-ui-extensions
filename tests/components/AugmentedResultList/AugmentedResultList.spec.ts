bc9758c828479172d353a2facd5bf9b0ec32eb51
nlafrance-coveo <nlafrance@coveo.com>
2020-11-17 13:16:15 -0500

    feat(SFINT-3521): lint

--- tests/components/AugmentedResultList/AugmentedResultList.spec.ts
+++ tests/components/AugmentedResultList/AugmentedResultList.spec.ts
@@ -39,7 +39,7 @@ describe('AugmentedResultList', () => {
                 resultData,
                 commonData,
             },
-        }
+        };
     };
 
     const matchingIdField = '@id';
