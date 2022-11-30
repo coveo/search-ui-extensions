645a03710845f99487ae50b6790fd7e4058965fb
Michael Grondines <mgrondines@coveo.com>
2022-11-30 16:53:30 -0500

    fix dependencies (#116)
    SVCINT-1979

--- config/webpack.config.karma.js
+++ config/webpack.config.karma.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const WebpackKarmaDieHardPlugin = require('webpack-karma-die-hard');
 
 const webpackConfig = require('./webpack.config.js');
 // These modifications are required to have proper coverage with karma-coverage-istanbul-reporter.
@@ -44,12 +43,5 @@ webpackConfig.module.rules = [
 webpackConfig.externals.push({
     'coveo-search-ui-tests': 'CoveoJsSearchTests',
 });
-
-/**
- * Plugin for Webpack to ensure errors cause it to quit with a non-zero exit code
- * when used as a Karma preprocessor with the karma-webpack plugin.
- * This works around known issues in karma-webpack: https://github.com/webpack-contrib/karma-webpack/issues/66
- */
-webpackConfig.plugins.push(new WebpackKarmaDieHardPlugin());
 
 module.exports = webpackConfig;
